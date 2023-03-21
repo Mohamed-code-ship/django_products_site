857c5e0d099421494cd9c445ea55a90458fea646
Mohacode <mohaabdul568@gmail.com>
2023-03-21 18:52:46 +0300

    Last commit

--- demoproj/demoapp/models.py
+++ demoproj/demoapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=1000)
+    owner = models.CharField(max_length=1000)
+    price = models.IntegerField()
--- demoproj/demoapp/views.py
+++ demoproj/demoapp/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Product
 #create your views here.
 
 def index(request):
-    return  render (request, 'index.html')
+    products = Product.objects.all()
+    return  render (request, 'index.html',{'products':products})
 def name(request):
     name = request.POST['name']
     return  render (request, 'name.html',{'name':name})
